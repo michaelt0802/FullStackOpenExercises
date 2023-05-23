5640bc806a1ee557b6285033f0d16d6f58a6d07b
michaelt0802 <michaelt0802@gmail.com>
2023-05-23 11:58:39 -0700

    completed up to step3

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,9 +1,21 @@
 import { useState } from 'react'
 
+const Stats = ({good, neutral, bad}) => {
+  const all = good + neutral + bad;
+  const average = (good + bad * -1) / all;
+  const positive = good / all * 100;
+  return (
+  <>
+    all {all} <br />
+    average {average} <br />
+    positive {positive}% <br />
+  </>
+  )
+}
 
-const Button = (props) => (
-  <button onClick = {props.handleClick}>
-    {props.text}
+const Button = ({text, handleClick}) => (
+  <button onClick = {handleClick}>
+    {text}
   </button>
 )
 
@@ -23,9 +35,14 @@ const App = () => {
       <h1>Statistics</h1>
       <div>
 
-        <p>good {good} <br />
-        neutral {neutral} <br />
-        bad {bad}</p>
+        <p>
+          good {good} <br />
+          neutral {neutral} <br />
+          bad {bad} <br />
+          <Stats good={good} neutral={neutral} bad={bad}/>
+        </p>
+
+
       </div>
 
     </div>
