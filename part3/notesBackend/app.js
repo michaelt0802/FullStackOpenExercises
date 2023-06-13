24f5c001cedfe97138e1cb4307bb9ddbcd758fad
michaelt0802 <michaelt0802@gmail.com>
2023-06-13 13:23:11 -0700

    added jest testing library to notes and added some tests

--- part3/notesBackend/app.js
+++ part3/notesBackend/app.js
@@ -1,19 +1,18 @@
-const config = require('./util/config')
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const notesRouter = require('/controllers/notes')
+const notesRouter = require('./controllers/notes')
 const middleware = require('./utils/middleware')
-const logger = require('./util/logger')
+const logger = require('./utils/logger')
 const mongoose = require('mongoose')
-
 mongoose.set('strictQuery', false)
 
-logger.info('connecting to', config.MONGOD_URI)
+logger.info('connecting to', config.MONGODB_URI)
 
-mongoose.connect(config.MONGOD_URI)
+mongoose.connect(config.MONGODB_URI)
   .then(() => {
-    logger.info('connect to MongoDB')
+    logger.info('connected to MongoDB')
   })
   .catch((error) => {
     logger.error('error connecting to MongoDB:', error.message)
@@ -23,6 +22,7 @@ app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
+
 app.use('/api/notes', notesRouter)
 
 app.use(middleware.unknownEndpoint)
