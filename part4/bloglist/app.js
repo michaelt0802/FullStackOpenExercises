366105cf5ceab8f87d1d4d13bd9fb47ebee4b618
michaelt0802 <michaelt0802@gmail.com>
2023-06-13 15:18:50 -0700

    practice writing unit tests and functions using lodash library

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -5,7 +5,7 @@ const blogsRouter = require('./controllers/blogs')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
-mongoose.set('strictQuery', false)
+// mongoose.set('strictQuery', false)
 
 logger.info('connecting to', config.MONGODB_URI)
 
---
+++ part4/bloglist/utils/list_helper.js
@@ -0,0 +1,56 @@
+var _ = require('lodash');
+
+const dummy = (blogs) => {
+  return 1;
+}
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((accum, item) => {
+    return accum + item.likes;
+  }, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((mostLikes, item) => {
+    return mostLikes.likes < item.likes
+    ? {
+        title: item.title,
+        author: item.author,
+        likes: item.likes
+      }
+    : mostLikes
+  }, {
+    likes: -1
+})
+}
+
+
+const mostBlogs = (blogs) => {
+  const authors = _.countBy(blogs, 'author')
+  const max = _.maxBy(_.keys(authors), (x) => authors[x])
+
+
+  return {
+    author: max,
+    blogs: authors[max]
+  }
+}
+
+const mostLikes = (blogs) => {
+  const likes = _.groupBy(blogs, 'author')
+
+  const authorWithMostLikes = _.maxBy(_.keys(likes), author => _.sumBy(likes[author], 'likes'))
+
+  return {
+    author: authorWithMostLikes,
+    likes: _.sumBy(likes[authorWithMostLikes], 'likes')
+  }
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
+}
