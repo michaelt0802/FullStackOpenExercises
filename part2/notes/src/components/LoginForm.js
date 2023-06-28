f83232678968431e4ce97635d1beafa2f8c91d2b
michaelt0802 <michaelt0802@gmail.com>
2023-06-28 11:21:49 -0700

    added testing functionality for front end and fixed linter errors

--- part2/notes/src/components/LoginForm.js
+++ part2/notes/src/components/LoginForm.js
@@ -1,34 +1,46 @@
+import PropTypes from 'prop-types'
+
 const LoginForm = ({
   handleSubmit,
   handleUsernameChange,
   handlePasswordChange,
   username,
   password
- }) => {
- return (
-   <div>
-     <h2>Login</h2>
+}) => {
 
-     <form onSubmit={handleSubmit}>
-       <div>
+  LoginForm.prototype = {
+    handleSubmit: PropTypes.func.isRequired,
+    handleUsernameChange: PropTypes.func.isRequired,
+    handlePasswordChange: PropTypes.func.isRequired,
+    username: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired
+  }
+
+
+  return (
+    <div>
+      <h2>Login</h2>
+
+      <form onSubmit={handleSubmit}>
+        <div>
          username
-         <input
-           value={username}
-           onChange={handleUsernameChange}
-         />
-       </div>
-       <div>
+          <input
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div>
          password
-         <input
-           type="password"
-           value={password}
-           onChange={handlePasswordChange}
-         />
-     </div>
-       <button type="submit">login</button>
-     </form>
-   </div>
- )
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  )
 }
 
 export default LoginForm
---
+++ part2/notes/src/components/Note.test.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, screen } from '@testing-library/react'
+import Note from './Note'
+
+test('render content', () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    important: true
+  }
+
+  render(<Note note={note} />)
+
+  const element = screen.getByText('Component testing is done with react-testing-library')
+
+  expect(element).toBeDefined()
+
+
+})
