e86493f9eaddde92b09f07290f07cb3cfbf459f4
michaelt0802 <michaelt0802@gmail.com>
2023-06-30 12:09:51 -0700

    also combined reducers as well as implemented a filter to search through anecdotes

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -2,7 +2,14 @@ import { useSelector, useDispatch } from 'react-redux'
 import { vote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(({anecdotes, filter})=> {
+    if (!filter) {
+      return anecdotes
+    }
+    return anecdotes.filter(anecdote => anecdote.content.includes(filter))
+  })
+
+
   const dispatch = useDispatch()
 
   return (
