01a42d4dd95e230da216e6483f557f3860b815e3
michaelt0802 <michaelt0802@gmail.com>
2023-07-05 11:38:34 -0700

    refactored axios requests out of components and into reducer files. Also moved configureStore setup into seperate file

--- part6/noteswithredux/src/index.js
+++ part6/noteswithredux/src/index.js
@@ -2,25 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
-
+import store from './store'
 import App from './App'
 
 import filterReducer from './reducers/filterReducer'
 import noteReducer, { setNotes } from './reducers/noteReducer'
 import noteService from './services/notes'
 
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter: filterReducer
-  }
-})
 
-noteService.getAll().then(notes =>
-  store.dispatch(setNotes(notes))
-)
 
-console.log(store.getState())
+// noteService.getAll().then(notes =>
+//   store.dispatch(setNotes(notes))
+// )
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
