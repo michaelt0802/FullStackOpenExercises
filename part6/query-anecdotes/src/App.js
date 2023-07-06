3738fb5d1628f6a46669abc40f604cb5e1dd03ca
michaelt0802 <michaelt0802@gmail.com>
2023-07-06 14:20:52 -0700

    new anecdotes with react query. displays when loading or is error

---
+++ part6/query-anecdotes/src/App.js
@@ -0,0 +1,55 @@
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+import { useQuery  } from 'react-query'
+import { getAnecdotes } from './requests'
+const App = () => {
+
+  const handleVote = (anecdote) => {
+    console.log('vote')
+  }
+
+  // const anecdotes = [
+  //   {
+  //     "content": "If it hurts, do it more often",
+  //     "id": "47145",
+  //     "votes": 0
+  //   },
+  // ]
+
+  const { isLoading, isError, data, error } = useQuery('anecdotes', getAnecdotes)
+
+  // console.log('result', result)
+
+  if (isLoading) {
+    return <div>loading data...</div>
+  }
+
+  if (isError) {
+    return <div>Error: {error.message} <br/>anecdote service not available due to problems in server</div>
+  }
+
+  const anecdotes = data
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+
+      <Notification />
+      <AnecdoteForm />
+
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
