e8dfce05d99fe634768b581c227f2bc1ad48d1bb
michaelt0802 <michaelt0802@gmail.com>
2023-07-06 11:48:03 -0700

    now can update and createNew

--- part6/query-notes/requests.js
+++
@@ -1,3 +0,0 @@
-import axios from 'axios'
-
-export const getNotes = () => axios.get('http://localhost:3001/api/notes').then(res => res.data)
---
+++ part6/query-notes/src/requests.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3001/api/notes'
+
+export const getNotes = () =>
+  axios.get(baseUrl).then(res => res.data)
+
+export const createNote = newNote =>
+  axios.post(baseUrl, newNote).then(res => res.data)
+
+export const updateNote = updatedNote =>
+  axios.put(`${baseUrl}/${updateNote.id}`, updateNote).then(res => res.data)
