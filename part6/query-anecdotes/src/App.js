0b001ecd36d7bbd283b8a5a2fa94014be06e3f74
michaelt0802 <michaelt0802@gmail.com>
2023-07-06 14:34:59 -0700

    adding new anecdotes and voting on them now works

--- part6/query-anecdotes/src/App.js
+++ part6/query-anecdotes/src/App.js
@@ -1,24 +1,19 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
-import { useQuery  } from 'react-query'
-import { getAnecdotes } from './requests'
+import { useQuery, useMutation, QueryClient, useQueryClient  } from 'react-query'
+import { getAnecdotes, updateAnecdote } from './requests'
 const App = () => {
-
+  const queryClient = useQueryClient()
+  const updateAnecdoteMutation = useMutation(updateAnecdote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('anecdotes')
+    }
+  })
   const handleVote = (anecdote) => {
-    console.log('vote')
+    updateAnecdoteMutation.mutate({...anecdote, votes: anecdote.votes + 1})
   }
 
-  // const anecdotes = [
-  //   {
-  //     "content": "If it hurts, do it more often",
-  //     "id": "47145",
-  //     "votes": 0
-  //   },
-  // ]
-
   const { isLoading, isError, data, error } = useQuery('anecdotes', getAnecdotes)
-
-  // console.log('result', result)
 
   if (isLoading) {
     return <div>loading data...</div>
--- part6/query-anecdotes/src/components/AnecdoteForm.js
+++ part6/query-anecdotes/src/components/AnecdoteForm.js
@@ -1,10 +1,18 @@
+import { createAnecdote } from '../requests'
+import { useMutation, useQueryClient } from 'react-query'
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+  const newAnecdoteMutation = useMutation(createAnecdote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('anecdotes')
+    }
+  })
 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    console.log('new anecdote')
+    newAnecdoteMutation.mutate({content, votes: 0})
 }
 
   return (
