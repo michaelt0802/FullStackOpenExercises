93bd617c6214aeae6d77305cc1e69b8914837728
michaelt0802 <michaelt0802@gmail.com>
2023-08-31 15:37:48 -0700

    finished exercises and settuping express

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -1,3 +1,8 @@
+interface heightWeight {
+  height: number;
+  weight: number;
+}
+
 const calculateBmi = (height: number, weight: number): string => {
   const bmi = (weight / (height * height)) * 703
   if (bmi < 18.5) return 'Underweight'
@@ -6,4 +11,28 @@ const calculateBmi = (height: number, weight: number): string => {
   else return 'Obese'
 }
 
-console.log(calculateBmi(68, 180))
+const parseArguments = (args: string[]): heightWeight => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values not numbers');
+  }
+}
+
+try {
+  const { height, weight } = parseArguments(process.argv)
+  console.log(calculateBmi(height, weight))
+} catch (error: unknown) {
+  if (error instanceof Error) {
+    console.log('Error: ', error.message)
+  }
+}
+
+
+export {}
--- part9/exerciseCalculator.ts
+++ part9/exerciseCalculator.ts
@@ -23,4 +23,36 @@ const calculateExercises = (hours: number[], target: number): Result => {
   }
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+const parseArguments = (args: string[]): { inputArray, target } => {
+  if (args.length < 12) throw new Error('Not enough arguments');
+  if (args.length > 12) throw new Error('Too many arguments');
+
+  const inputArray = []
+  let target: number;
+  if (!isNaN(Number(args[2]))) {
+    target = Number(args[2])
+  } else {
+    throw new Error('Provided values not numbers');
+  }
+  for (let i = 3; i < 12; i++) {
+    if (!isNaN(Number(args[i]))) {
+      inputArray.push(Number(args[i]))
+    } else {
+      throw new Error('Provided values not numbers');
+    }
+  }
+
+  return { inputArray, target }
+}
+
+try {
+const { inputArray, target } = parseArguments(process.argv)
+console.log(calculateExercises(inputArray, target))
+} catch (error: unknown) {
+  if (error instanceof Error) {
+    console.log('Error: ', error.message)
+  }
+}
+
+
+export {}
